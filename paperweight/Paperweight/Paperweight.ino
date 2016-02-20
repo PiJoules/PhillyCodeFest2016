065121c74b0c51bf9879cf5e02c63d15540eaf96
Peter Gagliardi <ptrgags@live.com>
2016-02-20 16:28:14 -0500

    Add LCD Display
    Add LCD Display to the project.

--- paperweight/Paperweight/Paperweight.ino
+++ paperweight/Paperweight/Paperweight.ino
@@ -1,6 +1,12 @@
+#include <LiquidCrystal.h>
+
+//Shift Register Pins
 const int PIN_DATA = 4;
 const int PIN_LATCH = 3;
 const int PIN_CLOCK = 2;
+
+//Set up the LCD
+LiquidCrystal lcd(12, 11, 7, 8, 9, 10);
 
 void setup() {
     //Initialize Shift register
@@ -8,18 +14,39 @@ void setup() {
     pinMode(PIN_CLOCK, OUTPUT);
     pinMode(PIN_LATCH, OUTPUT);
 
+    //Clear Shift Register
+    digitalWrite(PIN_LATCH, LOW);
+    shiftOut(PIN_DATA, PIN_CLOCK, MSBFIRST, 0);
+    digitalWrite(PIN_LATCH, HIGH);
+
     //Initialize Serial Port
     Serial.begin(9600);
+
+    //Initialize LCD display
+    lcd.begin(16, 2);
+    lcd.setCursor(0, 0);
+    lcd.print("Loading...");
 }
 
 void loop() {
-    //Wait for a single byte
+    //Wait for the arrival status
     while (Serial.available() < 1);
     byte arrivalStatus = Serial.read();
-    Serial.write(arrivalStatus);
 
     //Update LEDs
     digitalWrite(PIN_LATCH, LOW);
     shiftOut(PIN_DATA, PIN_CLOCK, MSBFIRST, arrivalStatus);
     digitalWrite(PIN_LATCH, HIGH);
+
+    //Wait for the ETA of next bus
+    while (Serial.available() < 1);
+    byte eta = Serial.read();
+
+    //Update LCD
+    lcd.setCursor(0, 0);
+    lcd.print("Next bus arrives");
+    lcd.setCursor(0, 1);
+    lcd.print("in ");
+    lcd.print(eta);
+    lcd.print(" min.");
 }
