e7b24ebc9dcfa77142f0b9f5ada589d28bccdfd5
Peter Gagliardi <ptrgags@live.com>
2016-02-20 21:25:56 -0500

    Refactor paperweight.py
    Update paperweight.py to grab info from the config file and refactor
    everything into functions. Just need to actually send requests and
    test test test!

--- paperweight/paperweight.py
+++ paperweight/paperweight.py
@@ -3,41 +3,100 @@ import time
 import struct
 import random
 from serial import Serial
+from ConfigParser import ConfigParser
 
 #Request delay in seconds
 REQUEST_DELAY = 10
 
-#TODO: Read this from a config file
-STOP_ID = "699"
-ROUTE = "44"
-DIRECTION = "eastbound"
-PORT = "COM12"
-BAUD = 9600
+#Send this packet to the Arduino to say goodbye
+MESSAGE_BYE = b'\xFF\xFF\xFF'
 
-#Connect to Arduino
-print "Connecting to Arduino..."
-ser = Serial(PORT, BAUD)
-ser.read(1)
-print "Connected!"
+class BadResponse(Exception):
+    def __str__(self):
+        return "ERROR: {}".format(self.message)
 
-print "Press Ctrl + C to exit."
-try:
-    while True:
-        #Query the current status
-        #TODO: use requests!
-        data = {
-            'arrival_status': random.randint(0, 6),
-            'eta': random.randint(0, 600),
-        }
+def read_config(fname):
+    """
+    Read config from a file and return
+    the information.
 
-        arrival_status = data['arrival_status']
-        arrival_status = 0 if arrival_status == -1 else 1 << arrival_status
+    :rtype: dict, dict
+    :returns: (route_info, serial_info)
+    """
+    config = ConfigParser()
+    config.read(fname)
 
-        eta = data['eta']
+    #Get the route info
+    route_info = dict(config.items('route'))
 
-        data = struct.pack(">BH", arrival_status, eta)
-        ser.write(data)
-        time.sleep(REQUEST_DELAY)
-except KeyboardInterrupt:
-    print "Bye!"
-    ser.write("\xFF\xFF\xFF")
+    #Get the serial port info
+    serial_info = dict(config.items('serial'))
+    serial_info['baud'] = int(serial_info['baud'])
+
+    return route_info, serial_info
+
+def connect(serial_info):
+    """
+    Connect to Arduino and return a Serial instance.
+
+    :returns: Serial port connection
+    :rtype: serial.Serial
+    """
+    print "Connecting to Arduino..."
+    ser = Serial(serial_info['port'], serial_info['baud'])
+    ser.read(1) #Arduino sends a byte when connected
+    print "Connected!"
+    return ser
+
+def fetch_data(route_info):
+    """
+    Fetch data about a route
+
+    :param dict route_info: the information about
+        the route for sending to the Flask server.
+    """
+    #TODO: Use Requests
+    return {
+        'arrival_status': random.randint(-10, 10),
+        'eta': random.randint(0, 600),
+    }
+
+def pack_data(data):
+    """
+    Pack the message into a serial message
+
+    :param dict data: JSON data
+    :rtype: bytes
+    :returns: packet message suitable for sending via pySerial
+    """
+    #TODO: Add error checking
+    arrival_status = data['arrival_status']
+    if arrival_status > 6 or arrival_status < -1:
+        raise BadResponse("Invalid Arrival status: {}".format(arrival_status))
+    if arrival_status == -1:
+        arrival_status = 0
+    else:
+        arrival_status = 1 << arrival_status
+
+    eta = data['eta']
+
+    return struct.pack(">BH", arrival_status, eta)
+
+if __name__ == '__main__':
+    route_info, serial_info = read_config('paperweight.conf')
+    ser = connect(serial_info)
+
+    print "Press Ctrl + C to exit."
+    try:
+        while True:
+            try:
+                data = fetch_data(route_info)
+                packed = pack_data(data)
+                ser.write(packed)
+            except BadResponse as e:
+                print e
+            finally:
+                time.sleep(REQUEST_DELAY)
+    except KeyboardInterrupt:
+        print "Bye!"
+        ser.write(MESSAGE_BYE)
